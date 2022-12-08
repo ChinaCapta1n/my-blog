8c4f8b52dce098f1f9949ab0118723e8591e5599
Ryan Li <ryan.li@mirumagency.com>
2022-12-08 21:42:19 +0800

    router

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,10 +1,14 @@
 import React, { memo } from 'react';
+import { useRoutes } from 'react-router-dom';
+import routes from './router';
 
 const App = memo(() => {
   return (
       <div className="app">
         <div className="header">header</div>
-        <div className="content">content</div>
+        <div className="content">
+          { useRoutes(routes) }
+        </div>
         <div className="footer">footer</div>
       </div>
   )
--- frondend/src/index.js
+++ frondend/src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+
 import App from './App';
 
 import './assets/less/index.less';
@@ -7,6 +9,11 @@ import './assets/less/index.less';
 const root = ReactDOM.createRoot(document.getElementById('luffy'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="loading">
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
+    
   </React.StrictMode>
 );
