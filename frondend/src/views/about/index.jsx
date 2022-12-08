12d4b01ff9644d6d9dcf40c58ffea7c101210778
Ryan Li <ryan.li@mirumagency.com>
2022-12-08 21:25:43 +0800

    less

---
+++ frondend/src/views/about/index.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const About = memo(() => {
+    return (
+        <div className="about">
+            about
+        </div>
+    )
+})
+
+export default About;
---
+++ frondend/src/views/home/index.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Home = memo(() => {
+    return (
+        <div className="home">
+            home
+        </div>
+    )
+})
+
+export default Home;
